9d864ffaf6732068b180994fcaf9cdd7212845d9
narr07 <dinarpermadi07@gmail.com>
2025-02-11 17:26:36 +0700

    configure content module with D1 database and add initial cscs markdown file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint']
+  modules: ['@nuxt/content', '@nuxt/eslint'],
+  content: {
+    database: {
+      type: 'd1',
+      binding: 'tina'
+    }
+  }
 })
